4832c57c12c8bcdb7cb30c851c73d1e6ba8b4760
Vedat <muhvedatdemir@gmail.com>
2021-03-31 17:55:06 +0300

    final commit

---
+++ shop/src/app/product/product-add-forms2/product-add-forms2.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Product } from '../product';
+import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
+import { Category } from 'src/app/category/category';
+import { AlertifyService } from 'src/app/services/alertify.service';
+
+@Component({
+  selector: 'app-product-add-forms2',
+  templateUrl: './product-add-forms2.component.html',
+  styleUrls: ['./product-add-forms2.component.css'],
+  providers: [CategoryService, ProductService]
+})
+export class ProductAddForms2Component implements OnInit {
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private categoryService: CategoryService,
+    private productService:ProductService,
+    private alertifyService:AlertifyService
+  ) { }
+
+  productAddForm!: FormGroup;
+  product: Product = new Product();
+  categories!: Category[];
+
+
+
+  createproductAddForm() {
+    this.productAddForm = this.formBuilder.group({
+      name: ["", Validators.required],
+      description: ["", Validators.required],
+      imageUrl: ["", Validators.required],
+      price: ["", Validators.required],
+      categoryId: ["", Validators.required]
+
+    });
+  }
+
+  ngOnInit(): void {
+    this.createproductAddForm();
+    this.categoryService.getProducts().subscribe(data => {
+      this.categories = data
+    });
+  }
+
+  add() {
+    if (this.productAddForm.valid) {
+      this.product = Object.assign({}, this.productAddForm.value)
+    }
+
+    this.productService.addProduct(this.product).subscribe(data=>{
+      this.alertifyService.success(data.name + " Eklendi")
+    })
+  }
+
+}
---
+++ shop/src/app/services/account.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { User } from '../login/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  constructor() { }
+
+  loggedIn = false;
+  login(user:User):boolean{
+
+    if (user.userName=="vedat"&&user.password=="2556") {
+      
+      this.loggedIn = true;
+      localStorage.setItem("isLogged",user.userName);
+      return true;
+    }
+    return false;
+  }
+
+  isLoggedIn(){
+    return this.loggedIn;
+  }
+
+  logOut(){
+    localStorage.removeItem("isLogged");
+    this.loggedIn=false;
+  }
+
+}
